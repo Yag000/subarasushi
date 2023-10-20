dfca836e81f16eac78b05074e063a5ef8c732926
IGLESIAS VAZQUEZ yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-10-20 14:37:04 +0200

    feat: Add menu API

--- lib/cards.mli
+++ lib/cards.mli
@@ -29,3 +29,22 @@ type card =
   | Appetizer of appetizer
   | Special of special
   | Dessert of dessert
+
+(** Set of default menus for the game. *)
+type default_menu =
+  | MyFirstMeal
+  | SushiGo
+  | PartySampler
+  | MasterMenu
+  | PointsPlatter
+  | CutthroatCombo
+  | BigBanquet
+  | DinnerForTwo
+
+type menu =
+  sushi_roll * appetizer * appetizer * appetizer * special * special * dessert
+(** This type represents the menu of a restaurant. It does not include the [nigiri] cards since they are always available.
+There exists a set of default menus that are available to the player. The player can also create their own menu, creating an instance of this type. *)
+
+val menu_of_default_menu : default_menu -> menu
+(** Initializes a menu from a default menu. *)
